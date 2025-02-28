97a32e4a98f13d2ff094953834ee0ad7fd5739ca
Otto-re <otto-reifschneider@t-online.de>
2025-02-28 14:56:08 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,16 +8,21 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-# find name, diet, locations and if type there the type in json data
+with open("animals_template.html", "r") as file:
+    template_content = file.read()
+
+output_animals = ''
 for animal in animals_data:
-    if "name" in animal:
-        print(f"Name: {animal["name"]}") #show name of fox
-    if "characteristics" in animal and "diet" in animal["characteristics"]:
-        print(f"Diet: {animal["characteristics"]["diet"]}") #show diet of fox
-    if "locations" in animal:
-        print(f"Locations: {animal["locations"][0]}") #show location of fox
+    output_animals += f"Name: {animal['name']}\n"
+    output_animals += f"Diet: {animal['characteristics']['diet']}\n"
+    output_animals += f"Location: {animal['locations'][0]}\n"
+
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        print(f"Type: {animal["characteristics"]["type"]}") #show type of fox if its there elso dont show this
-    print() # a clear line for better readebles
+        output_animals += f"Type: {animal['characteristics']['type']}\n"
 
+    output_animals += "\n"
 
+html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output_animals)
+
+with open("animals.html", "w") as file:
+    file.write(html_content)
