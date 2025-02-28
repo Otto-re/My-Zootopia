0c4666599fd4b4abbc70b8fb8f35e16be5936685
Otto-re <otto-reifschneider@t-online.de>
2025-02-28 16:39:53 +0100

    make code more readeble

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,14 +6,9 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data('animals_data.json')
-
-with open("animals_template.html", "r") as file:
-    template_content = file.read()
-
-output_animals = ''
-for animal in animals_data:
-    output_animals += '<li class="cards__item">\n'
+# Funktion zur Serialisierung eines einzelnen Tieres
+def serialize_animal(animal):
+    output_animals = '<li class="cards__item">\n'
     output_animals += f"<div class={"card__title"}>{animal['name']}</div>\n"
     output_animals += '<p class="card__text">\n'
     output_animals += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
@@ -26,8 +21,22 @@ for animal in animals_data:
     output_animals += '</li>\n'
     output_animals += "\n"
 
+    return output_animals
 
+# JSON-Datei einlesen
+animals_data = load_data('animals_data.json')
+
+# HTML-Vorlage laden
+with open("animals_template.html", "r") as file:
+    template_content = file.read()
+
+output_animals = ''
+for animal in animals_data:
+    output_animals += serialize_animal(animal)
+
+# Platzhalter in der Vorlage ersetzen
 html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output_animals)
 
+#neue html schreiben
 with open("animals.html", "w") as file:
     file.write(html_content)
