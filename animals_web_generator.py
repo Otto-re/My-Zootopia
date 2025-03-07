a0d0c33570678fb9a452278c9456cbe75952d647
Otto-re <otto-reifschneider@t-online.de>
2025-03-07 13:20:41 +0100

    die bestehende zootopia geändert das die datein nicht mher aus der json datei gezogen werden sondern aus der api json datei entfernt da nicht mehr benötigt wird und das ganze mit monkeys anstat mit fox ausprobiert

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,15 +1,27 @@
 import json
+import requests
 
-# load the json data in read mode
-def load_data(file_path):
-    """Lädt eine JSON-Datei"""
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+API_KEY = "UEO0ZcD1Nu20dCIbhInemg==PqkFbhkJjFEQQdwo"
+BASE_URL = "https://api.api-ninjas.com/v1/animals"
+
+
+def fetch_animal_data(animal_name):
+    """Holt Tierdaten von der API"""
+    headers = {"X-Api-Key": API_KEY}
+    params = {"name": animal_name}
+
+    response = requests.get(BASE_URL, headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return []
 
 # Funktion zur Serialisierung eines einzelnen Tieres
 def serialize_animal(animal):
     output_animals = '<li class="cards__item">\n'
-    output_animals += f"<div class={"card__title"}>{animal['name']}</div>\n"
+    output_animals += f"<div class={'card__title'}>{animal['name']}</div>\n"
     output_animals += '<p class="card__text">\n'
     output_animals += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
     output_animals += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
@@ -23,20 +35,31 @@ def serialize_animal(animal):
 
     return output_animals
 
-# JSON-Datei einlesen
-animals_data = load_data('animals_data.json')
+def main():
+    animal_name = input("Enter a name of an animal: ")
+
+    animals_data = fetch_animal_data(animal_name)
+
+    if not animals_data:
+        print("Data not found")
+        return
 
 # HTML-Vorlage laden
-with open("animals_template.html", "r") as file:
-    template_content = file.read()
+    with open("animals.html", "r") as file:
+        template_content = file.read()
 
-output_animals = ''
-for animal in animals_data:
-    output_animals += serialize_animal(animal)
+    output_animals = ''
+    for animal in animals_data:
+        output_animals += serialize_animal(animal)
 
 # Platzhalter in der Vorlage ersetzen
-html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output_animals)
+    html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output_animals)
 
 #neue html schreiben
-with open("animals.html", "w") as file:
-    file.write(html_content)
+    with open("animals.html", "w") as file:
+        file.write(html_content)
+
+    print("Website was successfully generated to the file animals.html.")
+
+if __name__ == "__main__":
+    main()
